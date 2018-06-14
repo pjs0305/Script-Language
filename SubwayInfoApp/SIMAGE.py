5e880a2103258966060aeac05accefbb14987333
박진수 <pgs0350@naver.com>
2018-06-14 13:13:46 +0900

    노선도 이미지 완벽
    이겠지?

--- SubwayInfoApp/SIMAGE.py
+++ SubwayInfoApp/SIMAGE.py
@@ -3,16 +3,6 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 
 class ImageWindow:
-    # 이미지 크기별 리스트
-    Subway_Image = {}
-
-    # 이미지 크기별 위젯 리스트
-    tk_image = {}
-
-    # 현재 이미지 크기
-    w = []
-    h = []
-
     def __init__(self, mainWindow, txt):
         self.window = Toplevel(mainWindow)
         self.window.geometry("640x600")
@@ -26,13 +16,12 @@ class ImageWindow:
         self.image_h = 1958
 
         # 이미지 축소 시 간격
-        self.image_w_s = self.image_w // 10
-        self.image_h_s = self.image_h // 10
+        self.image_w_s = self.image_w // 6
+        self.image_h_s = self.image_h // 6
 
         # 이미지 프레임 크기
         self.imageFrame_w = 600
         self.imageFrame_h = 500
-
 
         # 노선도 이미지 표시할 프레임
         self.LineWindow = Frame(self.window, width=500, height=400)
@@ -42,7 +31,7 @@ class ImageWindow:
         self.ScaleVar = IntVar()
 
         self.scale = Scale(self.LineWindow, variable=self.ScaleVar, command=self.PhotoResize, orient="horizontal", showvalue=False, from_=0,
-                      to=7, length=self.imageFrame_w)
+                      to=3, length=self.imageFrame_w)
         self.scale.pack(side="top")
         # # # # # # # # # # # # # #
 
@@ -72,7 +61,7 @@ class ImageWindow:
         self.ImageFrame = Frame(self.LineWindow, width=self.imageFrame_w, height=self.imageFrame_h)
         self.ImageFrame.pack()
 
-        self.label = Label(self.ImageFrame, image=ImageWindow.tk_image[self.Text][0])
+        self.label = Label(self.ImageFrame, image=self.tk_image[0])
 
         self.SetPosition()
         # # # # # # # # # # # # # #
@@ -94,41 +83,41 @@ class ImageWindow:
         self.combobox.set("수도권 1호선")
 
     def SetPhoto(self, self1=1):
-        print(1)
         self.Text = self.combobox.get()
         self.SetPhotoList()
 
         value = self.scale.get()
 
-        self.label = Label(self.ImageFrame, image=ImageWindow.tk_image[self.Text][value])
+        self.label = Label(self.ImageFrame, image=self.tk_image[value])
         self.SetPosition()
 
     def SetPhotoList(self):
         ImageURL = 'Image\\' + self.Text + ' 노선도.jpg'
 
-        if not ImageWindow.Subway_Image.get(self.Text):
-            ImageWindow.Subway_Image[self.Text] = []
-            ImageWindow.tk_image[self.Text] = []
+        self.Subway_Image = []
+        self.tk_image = []
+        self.w = []
+        self.h = []
 
-            for i in range(0, 8):
-                # 이미지 추가
-                ImageWindow.Subway_Image[self.Text].append(Image.open(ImageURL).resize(
-                    (self.image_w - (i * self.image_w_s), self.image_h - (i * self.image_h_s)),
-                    Image.ANTIALIAS))
+        for i in range(0, 4):
+            # 이미지 추가
+            self.Subway_Image.append(Image.open(ImageURL).resize(
+                (self.image_w - (i * self.image_w_s), self.image_h - (i * self.image_h_s)),
+                Image.ANTIALIAS))
 
-                # 이미지 설정
-                ImageWindow.tk_image[self.Text].append(ImageTk.PhotoImage(ImageWindow.Subway_Image[self.Text][i]))
+            # 이미지 설정
+            self.tk_image.append(ImageTk.PhotoImage(self.Subway_Image[i]))
 
-                # 이미지 크기 설정
-                ImageWindow.w.append(ImageWindow.Subway_Image[self.Text][i].width)
-                ImageWindow.h.append(ImageWindow.Subway_Image[self.Text][i].height)
+            # 이미지 크기 설정
+            self.w.append(self.Subway_Image[i].width)
+            self.h.append(self.Subway_Image[i].height)
 
     def SetPosition(self):
         value = self.scale.get()
 
         # 이미지 크기와 윈도우 창 크기로 이미지 위치 계산
-        IPx = (ImageWindow.w[value] // 2) - (self.imageFrame_w // 2)
-        IPy = (ImageWindow.h[value] // 2) - (self.imageFrame_h // 2)
+        IPx = (self.w[value] // 2) - (self.imageFrame_w // 2)
+        IPy = (self.h[value] // 2) - (self.imageFrame_h // 2)
 
         # 이미지 크기와 스크롤바 중간 값으로 스크롤바를 움직일 때 얼만큼 이동할지 계산
         sx = IPx // 5
@@ -143,7 +132,7 @@ class ImageWindow:
     def PhotoResize(self, self2):
         value = self.scale.get()
 
-        self.label = Label(self.ImageFrame, image=ImageWindow.tk_image[self.Text][value])
+        self.label = Label(self.ImageFrame, image=self.tk_image[value])
 
         self.SetPosition()
 
